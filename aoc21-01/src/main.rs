f7790ee6827c39ab109de4dbd092b00e60891cad
Mahrgell <mahrgell87@gmail.com>
2021-12-03 22:08:23 +0100

    rustfmt

--- aoc21-01/src/main.rs
+++ aoc21-01/src/main.rs
@@ -2,7 +2,10 @@ use std::fs;
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Failed to read file.");
-    let numbers: Vec<_> = contents.lines().map(|l| l.parse::<i32>().unwrap()).collect();
+    let numbers: Vec<_> = contents
+        .lines()
+        .map(|l| l.parse::<i32>().unwrap())
+        .collect();
     let mut last_last_num = 999999;
     let mut last_num = 999999;
     let mut last_sliding_sum = 999999999;
@@ -12,7 +15,7 @@ fn main() {
         if n > last_num {
             incs += 1;
         }
-        let sum = last_last_num+last_num+n;
+        let sum = last_last_num + last_num + n;
         if sum > last_sliding_sum {
             sliding_incs += 1;
         }
--- aoc21-03/src/main.rs
+++ aoc21-03/src/main.rs
@@ -1,19 +1,22 @@
-use std::fs;
 use std::cmp::Ordering;
+use std::fs;
 
 fn filter_list(nums: &Vec<u64>, keep_most_frequent: bool, max_check: u64) -> u64 {
     let mut lnums = nums.clone();
     let mut check = max_check;
     while lnums.len() > 1 {
-        let occ = lnums.iter().filter(|&n| (n&check) != 0).count();
-        let mut cmp_val = match lnums.len().cmp(&(2*occ as usize)) {
+        let occ = lnums.iter().filter(|&n| (n & check) != 0).count();
+        let mut cmp_val = match lnums.len().cmp(&(2 * occ as usize)) {
             Ordering::Greater => 0,
-            _ => check
+            _ => check,
         };
         if !keep_most_frequent {
             cmp_val = check - cmp_val;
         }
-        lnums = lnums.into_iter().filter(|&n| (n&check) == cmp_val).collect();
+        lnums = lnums
+            .into_iter()
+            .filter(|&n| (n & check) == cmp_val)
+            .collect();
         check /= 2;
     }
     lnums[0]
@@ -21,23 +24,36 @@ fn filter_list(nums: &Vec<u64>, keep_most_frequent: bool, max_check: u64) -> u64
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Failed to read file.");
-    let nums : Vec<_> = contents.lines().map(|l| u64::from_str_radix(l, 2).unwrap()).collect();
+    let nums: Vec<_> = contents
+        .lines()
+        .map(|l| u64::from_str_radix(l, 2).unwrap())
+        .collect();
     let mut gamma = 0u64;
     let mut epsilon = 0u64;
     let mut check = 1;
     let max_num = nums.iter().max().unwrap();
     while &check < max_num {
-        let occ = nums.iter().filter(|&n| (n&check) != 0).count();
-        match nums.len().cmp(&(2*occ as usize)) {
+        let occ = nums.iter().filter(|&n| (n & check) != 0).count();
+        match nums.len().cmp(&(2 * occ as usize)) {
             Ordering::Less => gamma += check,
-            Ordering::Equal => {gamma += check; epsilon += check;},
-            Ordering::Greater => {epsilon += check;},
+            Ordering::Equal => {
+                gamma += check;
+                epsilon += check;
+            }
+            Ordering::Greater => {
+                epsilon += check;
+            }
         }
         check *= 2;
     }
-    println!("gamma = {}, eps= {}, prod = {}", gamma, epsilon, gamma*epsilon);
+    println!(
+        "gamma = {}, eps= {}, prod = {}",
+        gamma,
+        epsilon,
+        gamma * epsilon
+    );
 
-    let oxy = filter_list(&nums, true, check/2);
-    let co2 = filter_list(&nums, false, check/2);
-    println!("oxy {}, co2 {} , prod {}", oxy, co2, oxy*co2);
+    let oxy = filter_list(&nums, true, check / 2);
+    let co2 = filter_list(&nums, false, check / 2);
+    println!("oxy {}, co2 {} , prod {}", oxy, co2, oxy * co2);
 }
