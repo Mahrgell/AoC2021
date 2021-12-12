c29dde81bfc4e7208854c57ed15528c9fcb65b47
Mahrgell <mahrgell87@gmail.com>
2021-12-12 15:35:38 +0100

    fmt Day12

--- aoc21-12/src/main.rs
+++ aoc21-12/src/main.rs
@@ -1,7 +1,7 @@
-use std::fs;
 use std::collections::HashMap;
+use std::fs;
 
-fn number_of_pathes(connections: &HashMap<String, Vec<String>>, allow_twice : bool) -> u32 {
+fn number_of_pathes(connections: &HashMap<String, Vec<String>>, allow_twice: bool) -> u32 {
     let mut todo = vec![(String::new(), "start", allow_twice)];
     let mut solution_counter = 0;
     while let Some((path, last, twice_available)) = todo.pop() {
@@ -17,11 +17,9 @@ fn number_of_pathes(connections: &HashMap<String, Vec<String>>, allow_twice : bo
             if next.chars().nth(0).unwrap().is_lowercase() && path.contains(next) {
                 if new_ta {
                     new_ta = false;
-                }
-                else {
+                } else {
                     continue;
                 }
-                
             }
             let new_path = path.clone() + "-" + next;
             todo.push((new_path, &next, new_ta));
@@ -32,13 +30,22 @@ fn number_of_pathes(connections: &HashMap<String, Vec<String>>, allow_twice : bo
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Failed to read file.");
-    let mut connections : HashMap<String, Vec<String>> = HashMap::new();
+    let mut connections: HashMap<String, Vec<String>> = HashMap::new();
     for l in contents.lines() {
-        let words : Vec<_> = l.split('-').collect();
+        let words: Vec<_> = l.split('-').collect();
         for (from, to) in [(words[0], words[1]), (words[1], words[0])] {
-            connections.entry(String::from(from)).or_insert(Vec::new()).push(String::from(to));
+            connections
+                .entry(String::from(from))
+                .or_insert(Vec::new())
+                .push(String::from(to));
         }
     }
-    println!("Number of pathes: {}", number_of_pathes(&connections, false));
-    println!("Number of pathes with one double visit: {}", number_of_pathes(&connections, true));
+    println!(
+        "Number of pathes: {}",
+        number_of_pathes(&connections, false)
+    );
+    println!(
+        "Number of pathes with one double visit: {}",
+        number_of_pathes(&connections, true)
+    );
 }
